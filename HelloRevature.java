e7ec1b90218190f58f15c20ae67b04bbbb44a77f
p.vo <philvo1991@gmail.com>
2022-02-08 10:26:56 -0600

    added HelloRevature

---
+++ HelloRevature.java
@@ -0,0 +1,25 @@
+public class HelloRevature {
+	public static void main(String[] args) {
+		System.out.println("Hello Revature and team");
+	}
+}
+
+/*
+	Java compile to bytecode which is run within the Java Virtual Machine JVM
+	Virtual Machine: virtual machine is a walled-off operating system within an operating system
+	Because we are running in the Java Virtual Machine, Java is what we call "Write one, run anywhere"
+	The JVM is independent of the operatin system: so we can use Java the same way on Windows, Mac, Linux
+	
+	Benefits of Java:
+	Automatic garbage collection
+
+	Very safe, reusable
+
+	Java is one of those things that's popular because it's popular
+	(because Java has a very intense amount of standardization thanks to the JVM)
+
+	
+	The enviroment that Java exists in is consistent everywhere because of the JRE, JVM
+
+	
+*/
